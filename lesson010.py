37f9d4defa531e764de62a35325e87d6628055f3
Mark Peters <mapeters101@earthlink.net>
2025-04-21 18:15:20 -0400

    Work on lesson010.

---
+++ lesson010.py
@@ -0,0 +1,18 @@
+
+#print(5 X 5)
+print(5 * 5)
+print()
+print(5 * 6)
+print(5 + 6)
+print(5 - 6)
+print(10 / 2)
+print(10 ** 3)
+print(10 % 3)
+print()
+
+print(5 + 5 + 5)
+print(5 + 5 * 5)
+print((5 + 5) * 5)
+
+
+
